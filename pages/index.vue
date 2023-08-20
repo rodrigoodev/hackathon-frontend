43ac5f6be57647d6768b281195a965c27fe19014
lzhgf <felix.lzhg@gmail.com>
2023-08-20 05:05:11 -0300

    improvements: sign in and sign up page

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,89 @@
 <template>
   <div>
-   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-    crossorigin="anonymous">
-
-<div class="container" id="container">
-        <div class="form-container sign-up-container"  @submit.prevent="signUp">
-            <form action="#">
-                <h1>Criar Conta</h1>
-                <input v-model="signUpData.name" type="text" placeholder="Name" />
-                <input v-model="signUpData.email" type="email" placeholder="Email" />
-                <input v-model="signUpData.password" type="password" placeholder="Password" />
-                <button>Cadastrar</button>
-            </form>
-        </div>
-        <div class="form-container sign-in-container">
-             <form class="form-login"  @submit.prevent="signIn">
-                <h1>Entrar</h1>
-                <input v-model="signInData.email" type="email" name="email" placeholder="Entre com seu email" required>
-                <input v-model="signInData.password" type="password" name="password" placeholder="Entre com sua senha" required>
-                <a href="#">Forgot your password?</a>
-                <button>Entrar</button>
-            </form>
-        </div>
-        <div class="overlay-container">
-            <div class="overlay">
-                <div class="overlay-panel overlay-left">
-                    <h1>Bem-vindo novamente!</h1>
-                    <p>Para permanecer conectado conosco, por favor faça login com suas informações pessoais. </p>
-                    <button class="ghost" id="signIn">Entrar</button>
-                </div>
-                <div class="overlay-panel overlay-right">
-                    <h1>Hello, Friend!</h1>
-                    <p>Enter your personal details and start journey with us</p>
-                    <button class="ghost" id="signUp">Sign Up</button>
-                </div>
+    <div class="container" id="container">
+      <div class="form-container sign-up-container" @submit.prevent="signUp">
+        <form action="#">
+          <h1>Criar Conta</h1>
+          <div class="input-container">
+            <div>
+              <font-awesome-icon :icon="['fa-solid', 'user']" />
             </div>
+            <input v-model="signUpData.name" type="text" placeholder="Name" />
+          </div>
+          <div class="input-container">
+            <div>
+              <font-awesome-icon :icon="['fa-solid', 'at']" />
+            </div>
+            <input
+              v-model="signUpData.email"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div class="input-container">
+            <div>
+              <font-awesome-icon :icon="['fa-solid', 'lock']" />
+            </div>
+            <input
+              v-model="signUpData.password"
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <button>Cadastrar</button>
+        </form>
+      </div>
+      <div class="form-container sign-in-container">
+        <form class="form-login" @submit.prevent="signIn">
+          <h1>Entrar</h1>
+          <div class="input-container">
+            <div>
+              <font-awesome-icon :icon="['fa-solid', 'at']" />
+            </div>
+            <input
+              v-model="signInData.email"
+              type="email"
+              name="email"
+              placeholder="Entre com seu email"
+              required
+            />
+          </div>
+          <div class="input-container">
+            <div>
+              <font-awesome-icon
+                class="siderbar-icon"
+                :icon="['fa-solid', 'lock']"
+              />
+            </div>
+            <input
+              v-model="signInData.password"
+              type="password"
+              name="password"
+              placeholder="Entre com sua senha"
+              required
+            />
+          </div>
+          <button>Entrar</button>
+        </form>
+      </div>
+      <div class="overlay-container">
+        <div class="overlay">
+          <div class="overlay-panel overlay-left">
+            <h1>Bem-vindo novamente!</h1>
+            <p>
+              Para permanecer conectado conosco, por favor faça login com suas
+              informações pessoais.
+            </p>
+            <button class="ghost" id="signIn">Entrar</button>
+          </div>
+          <div class="overlay-panel overlay-right">
+            <h1>Olá, Amigo!</h1>
+            <p>Entre com suas informações e comece sua jornada conosco</p>
+            <button class="ghost" id="signUp">Cadastrar</button>
+          </div>
         </div>
+      </div>
     </div>
-
   </div>
 </template>
 
@@ -47,35 +92,47 @@ export default {
   data() {
     return {
       signInData: {
-        email: '',
-        password: ''
+        email: "",
+        password: "",
       },
       signUpData: {
-        name: '',
-        email: '',
-        password: ''
-      }
+        name: "",
+        email: "",
+        password: "",
+      },
     };
   },
   methods: {
     signIn() {
-      console.log('Login attempt with email:', this.signInData.email, 'and password:', this.signInData.password);
+      console.log(
+        "Login attempt with email:",
+        this.signInData.email,
+        "and password:",
+        this.signInData.password
+      );
     },
     signUp() {
-      console.log('name:', this.signUpData.name, 'email:', this.signUpData.email, 'and password:', this.signUpData.password);
-    }
+      console.log(
+        "name:",
+        this.signUpData.name,
+        "email:",
+        this.signUpData.email,
+        "and password:",
+        this.signUpData.password
+      );
+    },
   },
   mounted() {
-    const signUpButton = this.$el.querySelector('#signUp');
-    const signInButton = this.$el.querySelector('#signIn');
-    const container = this.$el.querySelector('#container');
+    const signUpButton = this.$el.querySelector("#signUp");
+    const signInButton = this.$el.querySelector("#signIn");
+    const container = this.$el.querySelector("#container");
 
-    signUpButton.addEventListener('click', () =>
-      container.classList.add('right-panel-active')
+    signUpButton.addEventListener("click", () =>
+      container.classList.add("right-panel-active")
     );
 
-    signInButton.addEventListener('click', () =>
-      container.classList.remove('right-panel-active')
+    signInButton.addEventListener("click", () =>
+      container.classList.remove("right-panel-active")
     );
   },
 };
@@ -83,220 +140,228 @@ export default {
 
 <style>
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 body {
-    font-family: 'Montserrat', sans-serif;
-    background: #f6f5f7;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    margin: -20px 0 50px;
-		margin-top: 20px;
+  font-family: "Montserrat", sans-serif;
+  background: #f6f5f7;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: -20px 0 50px;
+  margin-top: 20px;
 }
 
 h1 {
-    font-weight: bold;
-    margin: 0;
+  font-weight: bold;
+  margin: 0;
 }
 
 p {
-    font-size: 14px;
-    font-weight: 100;
-    line-height: 20px;
-    letter-spacing: .5px;
-    margin: 20px 0 30px;
+  font-size: 14px;
+  font-weight: 100;
+  line-height: 20px;
+  letter-spacing: 0.5px;
+  margin: 20px 0 30px;
 }
 
 span {
-    font-size: 12px;
+  font-size: 12px;
 }
 
 a {
-    color: #333;
-    font-size: 14px;
-    text-decoration: none;
-    margin: 15px 0;
+  color: #333;
+  font-size: 14px;
+  text-decoration: none;
+  margin: 15px 0;
 }
 
 .container {
-    background: #fff;
-    border-radius: 10px;
-    box-shadow: 0 14px 28px rgba(0, 0, 0, .2), 0 10px 10px rgba(0, 0, 0, .2);
-    position: relative;
-    overflow: hidden;
-    width: 768px;
-    max-width: 100%;
-    min-height: 480px;
+  background: #fff;
+  border-radius: 10px;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.2), 0 10px 10px rgba(0, 0, 0, 0.2);
+  position: relative;
+  overflow: hidden;
+  width: 768px;
+  max-width: 100%;
+  min-height: 480px;
 }
 
 .form-container form {
-    background: #fff;
-    display: flex;
-    flex-direction: column;
-    padding:  0 50px;
-    height: 100%;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+  padding: 0 50px;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 }
 
 .social-container {
-    margin: 20px 0;
+  margin: 20px 0;
 }
 
 .social-container a {
-    border: 1px solid #ddd;
-    border-radius: 50%;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0 5px;
-    height: 40px;
-    width: 40px;
+  border: 1px solid #ddd;
+  border-radius: 50%;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 5px;
+  height: 40px;
+  width: 40px;
 }
 
 .form-container input {
-    background: #eee;
-    border: none;
-    padding: 12px 15px;
-    margin: 8px 0;
-    width: 100%;
+  background: #eee;
+  border: none;
+  padding: 0 0 0 10px;
+  width: 100%;
+  outline: none;
+}
+
+.input-container {
+  background: #eee;
+  padding: 12px 15px;
+  margin: 8px 0;
+  display: flex;
+  border-radius: 10px;
 }
 
 button {
-    border-radius: 20px;
-    border: 1px solid #37a9cf;
-    background: #37A9CE;
-    color: #fff;
-    font-size: 12px;
-    font-weight: bold;
-    padding: 12px 45px;
-    letter-spacing: 1px;
-    text-transform: uppercase;
-    transition: transform 80ms ease-in;
+  border-radius: 20px;
+  border: 1px solid #37a9cf;
+  background: #37a9ce;
+  color: #fff;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 12px 45px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  transition: transform 80ms ease-in;
 }
 
 button:active {
-    transform: scale(.95);
+  transform: scale(0.95);
 }
 
 button:focus {
-    outline: none;
+  outline: none;
 }
 
 button.ghost {
-    background: transparent;
-    border-color: #fff;
+  background: transparent;
+  border-color: #fff;
 }
 
 .form-container {
-    position: absolute;
-    top: 0;
-    height: 100%;
-    transition: all .6s ease-in-out;
+  position: absolute;
+  top: 0;
+  height: 100%;
+  transition: all 0.6s ease-in-out;
 }
 
 .sign-in-container {
-    left: 0;
-    width: 50%;
-    z-index: 2;
+  left: 0;
+  width: 50%;
+  z-index: 2;
 }
 
 .sign-up-container {
-    left: 0;
-    width: 50%;
-    z-index: 1;
-    opacity: 0;
+  left: 0;
+  width: 50%;
+  z-index: 1;
+  opacity: 0;
 }
 
 .overlay-container {
-    position: absolute;
-    top: 0;
-    left: 50%;
-    width: 50%;
-    height: 100%;
-    overflow: hidden;
-    transition: transform .6s ease-in-out;
-    z-index: 100;
+  position: absolute;
+  top: 0;
+  left: 50%;
+  width: 50%;
+  height: 100%;
+  overflow: hidden;
+  transition: transform 0.6s ease-in-out;
+  z-index: 100;
 }
 
 .overlay {
-    background: #3498db;
-    background: linear-gradient(to right, #3498db, #37A9CE) no-repeat 0 0 / cover;
-    color: #fff;
-    position: relative;
-    left: -100%;
-    height: 100%;
-    width: 200%;
-    transform: translateY(0);
-    transition: transform .6s ease-in-out;
+  background: #3498db;
+  background: linear-gradient(to right, #3498db, #37a9ce) no-repeat 0 0 / cover;
+  color: #fff;
+  position: relative;
+  left: -100%;
+  height: 100%;
+  width: 200%;
+  transform: translateY(0);
+  transition: transform 0.6s ease-in-out;
 }
 
 .overlay-panel {
-    position: absolute;
-    top: 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 0 40px;
-    height: 100%;
-    width: 50%;
-    text-align: center;
-    transform: translateY(0);
-    transition: transform .6s ease-in-out;
+  position: absolute;
+  top: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 40px;
+  height: 100%;
+  width: 50%;
+  text-align: center;
+  transform: translateY(0);
+  transition: transform 0.6s ease-in-out;
 }
 
 .overlay-right {
-    right: 0;
-    transform: translateY(0);
+  right: 0;
+  transform: translateY(0);
 }
 
 .overlay-left {
-    transform: translateY(-20%);
+  transform: translateY(-20%);
 }
 
 /* Move signin to right */
 .container.right-panel-active .sign-in-container {
-    transform: translateY(100%);
+  transform: translateY(100%);
 }
 
 /* Move overlay to left */
 .container.right-panel-active .overlay-container {
-    transform: translateX(-100%);
+  transform: translateX(-100%);
 }
 
 /* Bring signup over signin */
 .container.right-panel-active .sign-up-container {
-    transform: translateX(100%);
-    opacity: 1;
-    z-index: 5;
+  transform: translateX(100%);
+  opacity: 1;
+  z-index: 5;
 }
 
 /* Move overlay back to right */
 .container.right-panel-active .overlay {
-    transform: translateX(50%);
+  transform: translateX(50%);
 }
 
 /* Bring back the text to center */
 .container.right-panel-active .overlay-left {
-    transform: translateY(0);
+  transform: translateY(0);
 }
 
 /* Same effect for right */
 .container.right-panel-active .overlay-right {
-    transform: translateY(20%);
+  transform: translateY(20%);
 }
 
 .icons {
-	display: flex;
-	width: 30px;
-	height: 30px;
-	letter-spacing: 15px;
-	align-items: center;
+  display: flex;
+  width: 30px;
+  height: 30px;
+  letter-spacing: 15px;
+  align-items: center;
 }
 </style>
--- plugins/FontAwesome.ts
+++ plugins/FontAwesome.ts
@@ -1,11 +1,15 @@
 import { library, config } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faGripVertical, faFileLines, faHouse, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faGripVertical, faFileLines, faHouse, faPlus, faUser, faLock, faAt } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faGripVertical);
 library.add(faFileLines);
 library.add(faHouse);
 library.add(faPlus);
+library.add(faUser);
+library.add(faLock);
+library.add(faAt);
+
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component("font-awesome-icon", FontAwesomeIcon);
